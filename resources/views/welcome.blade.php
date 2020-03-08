0db4c8316d06ed71789144ff9e3f94ff19d46a82
Hoang Tung Anh <tunganhh239@gmail.com>
2020-03-08 15:19:19 +0700

    Sửa home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,7 +20,7 @@
         <h1 id="tieude">Trang xem điểm thi nhanh nhất</h1>
         <div class="comment">Được cập nhật từ trang của trung tâm khảo thí</div>
         <img src="{{asset('images/la.png')}}" alt="">
-        <a href="">ahihi</a>
+
     </div>
 </body>
 @endsection
